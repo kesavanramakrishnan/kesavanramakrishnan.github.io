27714538bdcb1f4ca981305100a504803fe45536
kesavanramakrishnan <kesavan6002@gmail.com>
2025-07-23 01:11:56 -0700

    remove footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,6 @@
 const Footer = () => {
   return (
     <footer className="bg-primary py-8">
-      <div className="container mx-auto px-4 text-center text-light/70">
-        <p>&copy; 2024 Kesavan Ramakrishnan. All rights reserved.</p>
-      </div>
     </footer>
   );
 };
